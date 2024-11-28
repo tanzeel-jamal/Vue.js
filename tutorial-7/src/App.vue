669da07530225c679634b9d9711569f0b04208ab
Mohammad Tanzeel Jamal <mohdtanzeel7294@gmail.com>
2024-11-28 21:23:54 +0530

    done with evnet handling

---
+++ tutorial-7/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- <form>
+    <button @click.prevent="popUp('hi')">Click</button>
+  </form> -->
+
+  <!-- only call `submit` when the `key` is `Enter` -->
+  <!-- <input @keyup.enter="submit" /> -->
+
+  <!-- <input @input="handleInput($event)" /> -->
+
+  <!-- <input
+    @input="(e) => handleInput(e.target.value)"
+    @keyup.alt.enter="clear"
+    :value="name"
+  /> -->
+
+  <input
+    @input="(e) => (name = e.target.value)"
+    @keyup.alt.enter="clear"
+    :value="name"
+  />
+
+  {{ name }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    popUp(message) {
+      console.log(message);
+    },
+    submit() {
+      console.log("Form submitted");
+    },
+    // handleInput(value) {
+    //   this.name = value;
+    //   console.log("Input:", value);
+    // },
+    clear() {
+      console.log("Clearing input");
+      this.name = "";
+    },
+  },
+};
+</script>
