6b015c57257e84c617281ac3ef4b67b3263bd96c
Mohammad Tanzeel Jamal <mohdtanzeel7294@gmail.com>
2024-12-02 00:07:33 +0530

    lifecycle hook

---
+++ tutorial-10/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<template>
+  <Counter v-if="show" />
+  <button @click="show = !show">Toggle</button>
+</template>
---
+++ tutorial-10/src/components/Counter.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  beforeCreate() {
+    console.log("Before creating the component");
+  },
+  created() {
+    console.log("Component created");
+  },
+  beforeMount() {
+    console.log("Before mounting the component");
+  },
+  mounted() {
+    console.log("Component mounted");
+  },
+  beforeUpdate() {
+    console.log("Before updating the component");
+  },
+  updated() {
+    console.log("Component updated");
+  },
+  beforeUnmount() {
+    console.log("Before unmounting the component");
+  },
+  unmounted() {
+    console.log("Component unmounted");
+  },
+};
+</script>
+
+<template>
+  <h1>Tanzeel {{ counter }}</h1>
+  <br />
+  <button @click="counter++">increment</button>
+</template>
