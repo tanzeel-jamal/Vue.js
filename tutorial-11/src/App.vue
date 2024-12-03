81b91e20c3f214af4b4c66c6ff1c32e0dae7369b
Mohammad Tanzeel Jamal <mohdtanzeel7294@gmail.com>
2024-12-03 18:46:07 +0530

    props

---
+++ tutorial-11/src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import BlogPost from "./components/BlogPost.vue";
+export default {
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "My journey with Vue",
+          author: "Razi",
+          isValid: true,
+          tags: [1, 2, 3, 4, 5],
+        },
+        {
+          id: 2,
+          title: "Blogging with Vue",
+          author: "Rafe",
+          isValid: false,
+          tags: [1, 2, 3, 4, 5],
+        },
+        {
+          id: 3,
+          title: "Why Vue is so fun",
+          author: "Farhan",
+          isValid: true,
+          tags: [1, 2, 3, 4, 5],
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <BlogPost
+    v-for="post in posts"
+    :title="post.title + ' by ' + post.author"
+    :id="post.id"
+    :isValid="post.isValid"
+    :tags="post.tags"
+  />
+  <!-- <BlogPost :posts="posts" /> -->
+</template>
---
+++ tutorial-11/src/components/BlogPost.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  //   props: ["title", "id"],
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+    },
+    tags: {
+      type: Array,
+      //   validator(value) {
+      //     return value.every((tag) => typeof tag === "number");
+      //   },
+    },
+    posts: {
+      type: Object,
+    },
+  },
+  computed: {
+    transform() {
+      return this.title.toUpperCase();
+    },
+    // formattedTags() {
+    //   return this.tags.join(" ");
+    // },
+  },
+};
+</script>
+
+<template>
+  <h4>{{ id }} - {{ title }} - {{ isValid }} - {{ tags }}</h4>
+  <span v-for="tag in tags">{{ tag }}</span>
+  <!-- <h1>{{ transform }}</h1> -->
+  <!-- <h1>{{ posts }}</h1> -->
+</template>
