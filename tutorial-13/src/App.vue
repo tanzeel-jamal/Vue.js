3947bc3fb3bf87dcf4ae142c26474f140ce9f341
Mohammad Tanzeel Jamal <mohdtanzeel7294@gmail.com>
2024-12-26 13:17:39 +0530

    slot

---
+++ tutorial-13/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import Button from "./components/Button.vue";
+export default {
+  components: { Button },
+  data() {
+    return {
+      buttonText: "Click me",
+      show: "left",
+    };
+  },
+  methods: {
+    con(value) {
+      console.log("Button clicked", value);
+    },
+    toggle() {
+      this.buttonText =
+        this.buttonText === "Click me" ? "Changed content" : "Click me";
+    },
+  },
+};
+</script>
+
+<template>
+  <!-- <Button :text="'Click me'" @on-click="con" /> -->
+  <Button @on-click="con">
+    <!-- {{ buttonText }} -->
+    <template #[show]>rafe </template>
+
+    <template #center> center </template>
+
+    <!-- <template v-slot:right> <h1>Right</h1> </template> -->
+  </Button>
+  <button @click="toggle">Changed</button>
+  <button @click="show = 'right'">Toggle</button>
+</template>
---
+++ tutorial-13/src/components/Button.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  emits: ["onClick"],
+  props: {},
+};
+</script>
+
+<template>
+  <button @click="$emit('onClick', 'Tanzeel is my name')">
+    <!-- <slot type="submit">Tanzeel</slot> -->
+    <div style="border: 5px solid red" v-if="$slots['left']">
+      <slot name="left"></slot>
+    </div>
+    <slot name="center">CENTER</slot>
+    <slot name="right">RIGHT</slot>
+  </button>
+</template>
+
+<style scoped>
+/* .left {
+  border: 1px solid black;
+} */
+</style>
